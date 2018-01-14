6591424f53c45e79269c92a8d790f56d231c3caf
Ryoya Masuda <190372k@ugs.kochi-tech.ac.jp>
2018-01-14 14:38:09 +0900

    few update

--- Menu/Menu_edit.php
+++ Menu/Menu_edit.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
+<title>ふらっとやまだ: お品書き編集</title>
 <meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="Menu.css"/>
 </head>
 
-<center>
-<h1> メニュー編集 </h1>
+<body>
+<div id="site-box">
 
+<center>
+<div id="title">
+<h1> お品書き編集 </h1>
+</div>
+</center>
+
+<div id="description">
 <?php
 if ($_REQUEST['menu'] == '') {
 	print('戻るボタンを押してメニューを選択してください。');
@@ -16,17 +25,33 @@ foreach($_REQUEST['menu'] as $reserve) {
 	
 }
 ?>
+</div>
+
+<div id="info">
+<form>
+<dl>
+
+<dt>お品書き名</dt>
+<dd><input id="name" name="tenpo" type="text" id="tenpo" size="50" maxlength="32" /></dd>
 <dt>概要</dt>
 <dd>
 <textarea name="about" id="about" cols="50" rows="10"></textarea>
 </dd>
 </dl>
 </form>
+</div>
 
-<form action="./Menu.html" method="post">
-<input type="submit" value="戻る" id="bt"/>
+<div id="update">
+<form action="./Menu_update.php" method="post">
+<input id="update_button" type="submit" value="同期" >
 </form>
+</div>
 
-</center>
+<div id="back">
+<form action="./Menu.html" method="post">
+<input id="back_button" type="submit" value="戻る" >
+</form>
+</div>
 
-
+</body>
+</html>
